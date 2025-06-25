01345616c6b4c167fe3e44b4599272c6219bc7fd
Maxwell-Calhoun <mcalh16@gmail.com>
2025-06-24 18:43:27 -0700

    addign and testing plex data wrangling

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import discord
 import os
 import threading
 import asyncio
+import json
 from fastapi import FastAPI, Request
 from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv()
-token = os.getenv("DISCORD_TOKEN")
+discord_token = os.getenv("DISCORD_TOKEN")
 channel_id = int(os.getenv("CHANNEL_ID"))
+plex_token = os.getenv("X-PLEX-TOKEN")
+hostname = os.getenv("PLEX_HOSTNAME")
 
 # discord scripts
 
@@ -38,7 +41,7 @@ intents.message_content = True
 client = MyClient(intents=intents)
 
 # Thread for the discord client
-discord_thread = threading.Thread(target=lambda: client.run(token), daemon=True)
+discord_thread = threading.Thread(target=lambda: client.run(discord_token), daemon=True)
 discord_thread.start()
 
 # API Under Here
@@ -50,12 +53,40 @@ async def root():
     return {"message": "Hello World"}
 
 # Will take in plex data and handle that to allow the discord bot to post appropriately
-@app.get("/plex")
+# TODO: need to proper web reponses later thiis will do for now
+@app.post("/plex")
 async def plex(request: Request):
-    print(str(request))
-    future = asyncio.run_coroutine_threadsafe(client.send_plex_update(), client.loop)
+    #print(await request.body)
+    form = await request.form()
+    print("Form keys:", list(form.keys()))
+    payload = form.get("payload")
+    if payload is None:
+        print("ERROR: No payload found in form data.")
+        return {"error": "No payload found"}
+
     try:
-        future.result(timeout=10)
-        return {"message": "Notification sent"}
-    except Exception as e:
-        return {"error": f"Failed to send message: {str(e)}"}
+        data = json.loads(payload)
+    except json.JSONDecodeError:
+        print("ERROR: Invalid JSON in payload.")
+        return {"error": "Invalid JSON"}
+
+    event = data.get("event")
+    metadata = data.get("Metadata")
+
+    print("Event:", event)
+
+    if event == "re":
+        future = asyncio.run_coroutine_threadsafe(client.send_plex_update(), client.loop)
+        
+        try:
+            future.result(timeout=10)
+            return {"message": "Notification sent"}
+        except Exception as e:
+            return {"error": f"Failed to send message: {str(e)}"}
+    else:
+        try:
+            
+            return {"message": "Ignoring due to wrong event"}
+        except Exception as e:
+            print(e)
+    
