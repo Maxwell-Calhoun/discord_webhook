6d88955a1b35dd4154c8144c8a4dbf9c3b802515
Maxwell-Calhoun <mcalh16@gmail.com>
2025-06-26 21:02:20 -0700

    making send plex more generic and implmenting shows to be now sent from webhook

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ discord_token = os.getenv("DISCORD_TOKEN")
 channel_id = int(os.getenv("CHANNEL_ID"))
 plex_token = os.getenv("X-PLEX-TOKEN")
 hostname = os.getenv("PLEX_HOSTNAME")
+thumbnail_url = os.getenv("THUMBNAIL_URL")
 
 app = FastAPI()
 
@@ -29,7 +30,7 @@ class MyClient(discord.Client):
         if message.content == 'ping':
             await message.channel.send('pong')
 
-    async def send_plex_movie_update(self, data):
+    async def send_plex_new_content(self, data):
         await self.wait_until_ready()
         print("Sending log")
         channel = self.get_channel(channel_id)
@@ -38,41 +39,40 @@ class MyClient(discord.Client):
             return
 
         thumb_url = f"https://{hostname}{data['thumb']}?X-Plex-Token={plex_token}"
-
-        embed = discord.Embed(
-            title=f"New {data['type']} to goon to!",
-            colour=discord.Colour.dark_teal(),
-            description=(
-                    f"**Title: **{data['title']}\n"
-                    f"**Tag: **{data.get('tagline', 'No tagline')}\n"
-                    f"**Starring: **{data.get('actors', 'No tagline')}\n"
-                    f"**Audience Rating: **{data.get('audience_rating', 'No tagline')}\n"
-                    f"**Rating: **{data.get('content_rating', 'No tagline')} / 10\n"
-                    f"**Genre: **{data.get('genres', 'No tagline')}\n"
-        ))
-
-        #embed.add_field(
-        #    name="Starring",
-        #    value=', '.join(data.get('actors', [])) or "N/A",
-        #    inline=False
-        #)
+        
+        if (data['type'] == "movie"):
+            embed = discord.Embed(
+                title=f"New {data['type']} to goon to!",
+                colour=discord.Colour.dark_teal(),
+                description=(
+                        f"**Title: **{data['title']}\n"
+                        f"**Tag: **{data.get('tagline', 'N/A')}\n"
+                        f"**Starring: **{data['actors']}\n"
+                        f"**Audience Rating: **{data.get('audience_rating', 'N/A')}\n"
+                        f"**Rating: **{data.get('content_rating', 'N/A')} / 10\n"
+                        f"**Genre: **{data.get('genres', 'N/A')}\n"
+            ))
+        else:
+            embed = discord.Embed(
+                title=f"New {data['type']} to goon to!",
+                colour=discord.Colour.dark_teal(),
+                description=(
+                        f"**Title: **{data['grandparentTitle']}\n"
+                        f"**Episode: **S{data['season']:02}E{data['episode']:02}: {data['title']}\n"
+                        f"**Summary: **{data.get('summary', 'N/A')}\n"
+                        f"**Starring: **{', '.join(data.get('actors'))}\n"
+                        f"**Audience Rating: **{data.get('audience_rating', 'N/A')}\n"
+                        f"**Rating: **{data.get('content_rating', 'N/A')} / 10\n"
+                        f"**Air Date:** {data['air_date']} | "
+                        f"**Duration:** {data['duration']} min"
+            ))
         
         embed.set_footer(text="Plex Library • GoonBox")
         # set image give larger image than thumbnail
+        embed.set_thumbnail(url=thumbnail_url)
         embed.set_image(url=thumb_url)
         await channel.send(embed=embed)
         print("Embed sent.")
-
-
-    # TODO: Implement this for shows        
-    async def send_plex_show_update(self, data):
-        print("Sending log")
-        channel = self.get_channel(channel_id)
-        if channel is None:
-            print("Channel not found. Bot may not have access or isn't in the server.")
-            return
-        message = "New " + data["type"] + " to goon to " + data["title"] + "! " + hostname + data["thumb"] + plex_token
-        await channel.send(embed=message)
 
 # Initialize the bot
 intents = discord.Intents.default()
@@ -97,10 +97,10 @@ async def test():
         "title" : "Solo: A Star Wars Story",
         "type" : "movie",
         "year" : "2018",
-        "audience_rating" : "6.8",
+        "audience_rating" : "6.6",
         "content_rating" : "PG-13",
         "tagline" : "Never tell him the odds",
-        "thumb" : "/library/metadata/7933/thumb/1750901990",
+        "thumb" : "/library/metadata/8008/thumb/1750992579",
         "actors" : actors
     }
 
@@ -111,7 +111,6 @@ async def test():
 # TODO: need to proper web reponses later thiis will do for now
 @app.post("/plex")
 async def plex(request: Request):
-    #print(await request.body)
     form = await request.form()
     print("Form keys:", list(form.keys()))
     payload = form.get("payload")
@@ -132,10 +131,10 @@ async def plex(request: Request):
         if event == "library.new":
             if metadata.get("type") == "movie":
                 data = wrangle_plex_new_movie_item(metadata)
-                client.loop.create_task(client.send_plex_movie_update(data))
             else:
                 data = wrangle_plex_new_tv_item(metadata)
-                client.loop.create_task(client.send_plex_show_update(data))
+            
+            client.loop.create_task(client.send_plex_new_content(data))
             
             return {"message": "Notification sent"} 
         return {"message": f"Ignoring webhook as it is not new library {event}"}
@@ -144,7 +143,7 @@ async def plex(request: Request):
     
 
 def wrangle_plex_new_movie_item(data):
-    wrangled = {
+    return {
         "title" : data.get("title"),
         "type" : data.get("type"),
         "tagline" : data.get("tagline"),
@@ -153,18 +152,25 @@ def wrangle_plex_new_movie_item(data):
         "thumb" : data.get("thumb"),
         "year" : data.get("year"),
         "genres" : [genre["tag"] for genre in data.get("Genre", [])],
-        "actors" : [actor["actor"] for actor in data.get("Role", [])][:3] 
+        "actors" : [actor["tag"] for actor in data.get("Role", [])][:3] 
     }
-    return wrangled
 
 def wrangle_plex_new_tv_item(data):
-    wrangled = {
-        "title" : data.get("title"),
-        "type" : data.get("type"),
-        "thumb" : data.get("thumb"),
-        "year" : data.get("year")
+    actors = ", ".join([actor["tag"] for actor in data.get("Role", [])][:3])
+    return {
+        "title": data.get("title"),
+        "type": data.get("type"),
+        "season": data.get("parentIndex"),
+        "episode": data.get("index"),
+        "grandparentTitle": data.get("grandparentTitle"),
+        "summary": data.get("summary", "N/A"),
+        "audience_rating" : data.get("audienceRating", "N/A"),
+        "content_rating": data.get("contentRating", "N/A"),
+        "air_date": data.get("originallyAvailableAt", "Unknown"),
+        "duration": round(int(data.get("duration", 0)) / 60000),  # ms → min
+        "thumb": data.get("thumb", data.get("grandparentThumb")),
+        "actors": actors if actors else "N/A"
     }
-    return wrangled
 
 if __name__ == "__main__":
     asyncio.run(startup())
