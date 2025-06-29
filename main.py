5ffbe3ff2bef70f728e2234bd125c829e702626a
Maxwell-Calhoun <mcalh16@gmail.com>
2025-06-29 14:49:49 -0700

    making data wrangling generic and not specific to event types

--- main.py
+++ main.py
@@ -47,18 +47,24 @@ class MyClient(discord.Client):
                         f"**Title: **{data['title']}\n"
                         f"**Tag: **{data.get('tagline', 'N/A')}\n"
                         f"**Starring: **{data['actors']}\n"
+                        f"**Runtime: **{data['duration']}\n"
                         f"**Audience Rating: **{data.get('audience_rating', 'N/A')}\n"
                         f"**Rating: **{data.get('content_rating', 'N/A')} / 10\n"
                         f"**Genre: **{data.get('genres', 'N/A')}\n"
             ))
         else:
+            # condition where input is not an episode but rather new show
+            if data['season'] is not None or data['episode'] is not None:
+                episode = f"**Episode: **S{data['season']:02}E{data['episode']:02}: {data['grandparentTitle']}\n"
+            else:
+                episode = ""
+
             embed = discord.Embed(
                 title=f"New {data['type']} to goon to!",
                 colour=discord.Colour.dark_teal(),
                 description=(
                         f"**Title: **{data['grandparentTitle']}\n"
-                        f"**Episode: **S{data['season']:02}E{data['episode']:02}: {data['grandparentTitle']}\n"
-                        f"**Summary: **{data.get('summary', 'N/A')}\n"
+                        f"{episode}"
                         f"**Starring: **{data.get('actors', 'N/A')}\n"
                         f"**Audience Rating: **{data.get('audience_rating', 'N/A')}\n"
                         f"**Rating: **{data.get('content_rating', 'N/A')} / 10\n"
@@ -91,7 +97,6 @@ async def root():
 # Test Base Root
 @app.get("/test")
 async def test():
-    actors = ["Alden Ehrenreich", "Joonas Suotamo", "Woody Harrelson"]
     data = {
         "title" : "Solo: A Star Wars Story",
         "type" : "movie",
@@ -99,8 +104,10 @@ async def test():
         "audience_rating" : "6.6",
         "content_rating" : "PG-13",
         "tagline" : "Never tell him the odds",
+        "duration" : "8100000",
         "thumb" : "/library/metadata/8008/thumb/1750992579",
-        "actors" : actors
+        "genres" : ["action", "adventure", "mystery"],
+        "actors" : ["Alden Ehrenreich", "Joonas Suotamo", "Woody Harrelson"]
     }
 
     client.loop.create_task(client.send_plex_new_content(data))
@@ -129,10 +136,7 @@ async def plex(request: Request):
 
     try:
         if event == "library.new":
-            if metadata.get("type") == "movie":
-                data = wrangle_plex_new_movie_item(metadata)
-            else:
-                data = wrangle_plex_new_tv_item(metadata)
+            data = wrangle_plex_payload(metadata)
             
             client.loop.create_task(client.send_plex_new_content(data))
             
@@ -140,36 +144,24 @@ async def plex(request: Request):
         return {"message": f"Ignoring webhook as it is not new library {event}"}
     except Exception as e:
             return {"error": f"Failed to send message: {str(e)}"}
-    
 
-# TODO: Combine the data handling as it doesnt need to be two seperate functions just handle the data potentially not being there
-def wrangle_plex_new_movie_item(data):
+def wrangle_plex_payload(data):
     actors = ", ".join([actor["tag"] for actor in data.get("Role", [])][:3])
+    genres = ", ".join([genre["tag"] for genre in data.get("Genre", [])][:3])
     return {
-        "title" : data.get("title"),
-        "type" : data.get("type"),
-        "tagline" : data.get("tagline"),
-        "content_rating" : data.get("contentRating"),
-        "audience_rating" : data.get("audienceRating"),
-        "thumb" : data.get("thumb"),
-        "year" : data.get("year"),
-        "genres" : [genre["tag"] for genre in data.get("Genre", [])],
-        "actors": actors if actors else "N/A"
-    }
-
-def wrangle_plex_new_tv_item(data):
-    actors = ", ".join([actor["tag"] for actor in data.get("Role", [])][:3])
-    return {
+        "grandparentTitle": data.get("grandparentTitle") or data.get("title") or "N/A",
         "type": data.get("type"),
         "season": data.get("parentIndex") or "N/A",
         "episode": data.get("index") or "N/A",
-        "grandparentTitle": data.get("grandparentTitle") or data.get("title") or "N/A",
-        "summary": data.get("summary", "N/A"),
-        "audience_rating" : data.get("audienceRating", "N/A"),
-        "content_rating": data.get("contentRating", "N/A"),
-        "air_date": data.get("originallyAvailableAt", "N/A"),
-        "duration": round(int(data.get("duration", 0)) / 60000),  # ms → min
-        "thumb": data.get("thumb", data.get("grandparentThumb")),
+        "tagline" : data.get("tagline"),
+        "summary": data.get("summary") or  "N/A",
+        "audience_rating" : data.get("audienceRating") or  "N/A",
+        "content_rating": data.get("contentRating") or  "N/A",
+        "air_date": data.get("originallyAvailableAt") or  "N/A",
+        "duration": round(int(data.get("duration", 0)) / 60000)  or  "N/A",  # ms → min
+        "thumb": data.get("thumb", data.get("grandparentThumb")) or  "N/A",
+        "year" : data.get("year") or "N/A",
+        "genres": genres if genres else "N/A",
         "actors": actors if actors else "N/A"
     }
 
